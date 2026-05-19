91c7139799ca040757dc9b398065ccf4b61c39ca
JhowQT <jhon.q.torrez@gmail.com>
2026-05-19 11:47:01 -0300

    classes dto de jwt

---
+++ FiapOrangeRoute/Auth/DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FiapOrangeRoute.Auth.DTOs;
+
+public class LoginDTO
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Senha { get; set; } = string.Empty;
+}
---
+++ FiapOrangeRoute/Auth/DTOs/TokenResponseDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace FiapOrangeRoute.Auth.DTOs;
+
+public class TokenResponseDTO
+{
+    public string Token { get; set; } = string.Empty;
+}
